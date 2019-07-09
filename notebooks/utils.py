fde1a3c8693d53a8e45d895d4d1d1ccbc2baeb5f
Gilang Ramadhan Ilhami <gigilangilhami@gmail.com>
2019-07-10 01:16:45 +0700

    Add utils file
    Add a util file to
    include functions to read
    and clean data.
    
    - Create a file called
      utils.py to include
      functios to read and
      clean data.
    - Add a function read
      and clean dataset.

---
+++ notebooks/utils.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import re
+
+def read_file(filepath): 
+    data = pd.read_excel(filepath)
+    return data
+
+def clean_text(data):
+
+    if isinstance(data, str):
+        clean = re.sub(r'\([^)]*\)', '', data)
+        clean = re.sub("\D","",clean)
+        
+        return clean
+
+    else:
+        return data
+
+def read_and_clean_data(filepath):
+
+    # Load the excel file
+    dataset = read_file(filepath)
+
+    # Get categorical variable
+    tot_cols = dataset.columns
+    num_cols = dataset._get_numeric_data().columns
+    cat_cols = list(set(tot_cols)-set(num_cols))
+
+    # Drop columns
+    drop_cols = ['NDB_No', 'Shrt_Desc']
+    dataset = dataset.drop(drop_cols, axis=1)
+
+    # Getting the labels
+    labels = ['Calcium_(mg)', 'Iron_(mg)','Zinc_(mg)', 
+    'Vit_A_IU', 'Vit_D_IU', 'Folate_Tot_(µg)']
+
+    # Remove text in GmWt_Desc2 and GmWt_Desc1
+    dataset[cat_cols[0]] = dataset[cat_cols[0]].apply(clean_text)
+    dataset[cat_cols[1]] = dataset[cat_cols[1]].apply(clean_text)
+
+    # Fill missing data with median
+    get_median = dataset.median()
+    dataset = dataset.fillna(get_median)
+
+    # List of names of the columns that we will drop for the input data
+    drop_cols = ['Vit_D_µg', 'Vit_A_RAE'] + labels
+
+    # Get the data for labels
+    y = dataset[labels].values
+
+    # Get the data for inputs
+    X = dataset.drop(drop_cols, axis=1).values
+
+    return X, y
